91bfbca09ed2ae2d2b73dec52a5951aa958ba7b3
Zoltán Tóth <zshun86@gmail.com>
2018-03-06 12:04:22 +0100

    Propertyselector extension tests added

---
+++ RediSearchSharp.Tests/PropertySelectorExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using RediSearchSharp.Utils;
+
+namespace RediSearchSharp.Tests
+{
+    [TestFixture]
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Local")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+    public class PropertySelectorExtensionsTests
+    {
+        public class GetMemberName
+        {
+            private class ChildType
+            {
+                public string ChildTypeProperty { get; set; }
+            }
+
+            private class TestType
+            {
+                public string TestTypeProperty { get; set; }
+                public ChildType ChildType { get; set; }
+            }
+
+            [Test]
+            public void Should_throw_when_the_property_selector_is_not_a_member_expression()
+            {
+                Expression<Func<TestType, string>> testExpression = t => t.ToString();
+                Assert.Throws<ArgumentException>(() =>
+                {
+                    testExpression.GetMemberName();
+                });
+            }
+
+            [Test]
+            public void Should_throw_when_the_expression_is_not_referring_to_a_property_of_the_type()
+            {
+                Expression<Func<TestType, string>> testExpression = t => t.ChildType.ChildTypeProperty;
+                Assert.Throws<ArgumentException>(() =>
+                {
+                    testExpression.GetMemberName();
+                });
+            }
+
+            [Test]
+            public void Should_return_the_property_name()
+            {
+                Expression<Func<TestType, string>> testExpression = t => t.TestTypeProperty;
+
+                string memberName = testExpression.GetMemberName();
+
+                Assert.That(memberName, Is.EqualTo("TestTypeProperty"));
+            }
+        }
+    }
+}
--- RediSearchSharp/Utils/PropertySelectorExtensions.cs
+++ RediSearchSharp/Utils/PropertySelectorExtensions.cs
@@ -5,7 +5,8 @@ namespace RediSearchSharp.Utils
 {
     public static class PropertySelectorExtensions
     {
-        public static string GetMemberName<TEntity, TProperty>(this Expression<Func<TEntity, TProperty>> propertySelector)
+        public static string GetMemberName<TEntity, TProperty>(
+            this Expression<Func<TEntity, TProperty>> propertySelector)
         {
             if (!(propertySelector.Body is MemberExpression memberExpression))
             {
@@ -14,13 +15,12 @@ namespace RediSearchSharp.Utils
 
             var memberInfo = memberExpression.Member;
 
-#if NETSTANDARD2_0 || NET45 || NET46
-                if (memberInfo.ReflectedType != typeof(TEntity) &&
-                    !typeof(TEntity).IsAssignableFrom(memberInfo.ReflectedType))
-                {
-                    throw new ArgumentException("Property selector does not refer to a property of the entity.");
-                }
-#endif
+            if (memberInfo.ReflectedType != typeof(TEntity) &&
+                !typeof(TEntity).IsAssignableFrom(memberInfo.ReflectedType))
+            {
+                throw new ArgumentException("Property selector does not refer to a property of the entity.");
+            }
+
             return memberInfo.Name;
         }
     }
