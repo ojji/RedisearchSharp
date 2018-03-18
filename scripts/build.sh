ec7c5eb96a0960e2236ceabbaf616fcd18717cda
Zoltán Tóth <zshun86@gmail.com>
2018-03-19 00:55:01 +0100

    Im not sure if im on the right path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 dotnet clean -c Release -o ./dist
-dotnet SonarScanner.MSBuild.dll begin /k:"ojji.redisearchsharp" /d:sonar.organization="ojji-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=$SONAR_TOKEN
 dotnet build ./RediSearchSharp.sln -c Release -o ./dist
-dotnet SonarScanner.MSBuild.dll end /d:sonar.login=$SONAR_TOKEN
