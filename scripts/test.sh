a7c02bb1b938044d26a6ff260d544caf4fd54ba9
Zoltán Tóth <zshun86@gmail.com>
2018-03-19 17:50:37 +0100

    Test results path

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dotnet test -c Release --logger:trx --no-build --no-restore ./RediSearchSharp.Tests/RediSearchSharp.Tests.csproj -o ./dist
+dotnet test -c Release --logger:trx --no-build --no-restore ./RediSearchSharp.Tests/RediSearchSharp.Tests.csproj -o ./dist -r "$TEST_RESULTS_PATH"
