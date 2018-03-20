5ecc5006e3c2717e5f187b56b96766925e345b1b
Zoltán Tóth <zshun86@gmail.com>
2018-03-20 11:00:25 +0100

    Forgot to update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dotnet test -c Release --logger:trx --no-build --no-restore ./RediSearchSharp.Tests/RediSearchSharp.Tests.csproj -o ./dist -r "$TEST_RESULTS_PATH"
+dotnet test -c Release --logger:trx --no-build --no-restore ./RediSearchSharp.Test/RediSearchSharp.Test.csproj -o ./dist -r "$TEST_RESULTS_PATH"
